8f8a56adfd2345be7bcf5bd0a4c86906ccbe23a8
Joint Solutions Technology <josstecnoo@gmail.com>
2024-04-21 17:14:18 +0700

    Comment template text

--- src/components/home/PopularGames/PopularGames.tsx
+++ src/components/home/PopularGames/PopularGames.tsx
@@ -7,50 +7,48 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 interface IPopularGamesProps {
-  popularGames: IGame[];
+    popularGames: IGame[];
 }
 
 const PopularGames: React.FC<IPopularGamesProps> = ({ popularGames }) => {
-  const [limit, setLimit] = useState(6);
-  const limitedPopularGames = popularGames.slice(0, limit);
+    const [limit, setLimit] = useState(6);
+    const limitedPopularGames = popularGames.slice(0, limit);
 
-  const handleClickExpandGame = () => {
-    if (limit > popularGames.length) {
-      setLimit(6);
-    } else {
-      setLimit((prevLimit) => prevLimit + 6);
-    }
-  };
-  return (
-    <Container className="mt-14 md:mt-16 lg:mt-24">
-      <div>
-        <h1 className="text-neutral-900 text-4xl lg:text-5xl font-bold text-center">
-          Sedang Populer
-        </h1>
-        <p className="text-neutral-800 lg:text-lg md:w-3/4 lg:w-2/3 mx-auto text-center mt-4">
+    const handleClickExpandGame = () => {
+        if (limit > popularGames.length) {
+            setLimit(6);
+        } else {
+            setLimit((prevLimit) => prevLimit + 6);
+        }
+    };
+    return (
+        <Container className="mt-14 md:mt-16 lg:mt-24">
+            <div>
+                <h1 className="text-neutral-900 text-4xl lg:text-5xl font-bold text-center">Sedang Populer</h1>
+                {/* <p className="text-neutral-800 lg:text-lg md:w-3/4 lg:w-2/3 mx-auto text-center mt-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo repellat
           quibusdam ad officiis, saepe modi consequuntur quae hic fugiat
           incidunt?
-        </p>
+        </p> */}
 
-        <div className="mt-10 grid grid-cols-auto-sm md:grid-cols-auto-md lg:grid-cols-auto-lg gap-3 lg:gap-6">
-          {limitedPopularGames.map((data) => (
-            <Game data={data} key={data.id} />
-          ))}
-        </div>
+                <div className="mt-10 grid grid-cols-auto-sm md:grid-cols-auto-md lg:grid-cols-auto-lg gap-3 lg:gap-6">
+                    {limitedPopularGames.map((data) => (
+                        <Game data={data} key={data.id} />
+                    ))}
+                </div>
 
-        {limit < popularGames.length && (
-          <button
-            onClick={handleClickExpandGame}
-            className="mx-auto flex items-center gap-2 bg-white mt-8 lg:text-sm text-primary-900 rounded-md py-2 px-4 font-semibold lg:font-medium hover:bg-primary-100"
-          >
-            Muat lebih banyak
-            <FontAwesomeIcon icon={faChevronDown} />
-          </button>
-        )}
-      </div>
-    </Container>
-  );
+                {limit < popularGames.length && (
+                    <button
+                        onClick={handleClickExpandGame}
+                        className="mx-auto flex items-center gap-2 bg-white mt-8 lg:text-sm text-primary-900 rounded-md py-2 px-4 font-semibold lg:font-medium hover:bg-primary-100"
+                    >
+                        Muat lebih banyak
+                        <FontAwesomeIcon icon={faChevronDown} />
+                    </button>
+                )}
+            </div>
+        </Container>
+    );
 };
 
 export default PopularGames;

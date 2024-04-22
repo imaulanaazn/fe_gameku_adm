907df8081ba9fbf52e8cdd76068263d892694dd6
irham maulana <i.maulana.azn@gmail.com>
2024-04-23 05:43:16 +0700

    change the min card size on sm screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,7 +36,7 @@ const config: Config = {
       gridTemplateColumns: {
         "auto-lg": "repeat(auto-fill, minmax(170px, 1fr))",
         "auto-md": "repeat(auto-fill, minmax(140px, 1fr))",
-        "auto-sm": "repeat(auto-fill, minmax(90px, 1fr))",
+        "auto-sm": "repeat(auto-fill, minmax(100px, 1fr))",
       },
     },
   },

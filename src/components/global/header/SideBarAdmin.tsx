9a729d7ca7364504c3b108b8f73a90f1c2a15d88
irham maulana <i.maulana.azn@gmail.com>
2024-05-01 13:21:49 +0700

    redesign sidebar

--- src/components/global/header/SideBarAdmin.tsx
+++ src/components/global/header/SideBarAdmin.tsx
@@ -1,24 +1,25 @@
 "use client";
 
 import {
-    faCogs,
-    faContactCard,
-    faCreditCard,
-    faCube,
-    faDoorOpen,
-    faFlag,
-    faGamepad,
-    faGlobeAsia,
-    faHistory,
-    faHome,
-    faImage,
-    faSortNumericUpAlt,
-    faTicket,
-    faTrash,
-    faUser,
+  faArrowLeft,
+  faArrowRight,
+  faCogs,
+  faContactCard,
+  faCreditCard,
+  faCube,
+  faDoorOpen,
+  faFlag,
+  faGamepad,
+  faGlobeAsia,
+  faHistory,
+  faHome,
+  faImage,
+  faSortNumericUpAlt,
+  faTicket,
+  faTrash,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -27,226 +28,279 @@ import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { useRecoilState } from "recoil";
 import { imageAtom } from "@/atom/logo";
 import { faDiagramProject } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const MENU = [
-    {
-        name: "Admin",
-        icon: faHome,
-        link: "/admin/admin",
-    },
-    {
-        name: "Dashboard",
-        icon: faHome,
-        link: "/admin",
-    },
-    {
-        name: "Konfigurasi",
-        icon: faCogs,
-        link: "/admin/configuration",
-    },
-    {
-        name: "Banner",
-        icon: faImage,
-        link: "/admin/banner",
-    },
-    {
-        name: "Kode Promo",
-        icon: faTicket,
-        link: "/admin/promo-code",
-    },
-    {
-        name: "User",
-        icon: faUser,
-        link: "/admin/user",
-    },
-    {
-        name: "Reseller",
-        icon: faDiagramProject,
-        link: "/admin/reseller",
-    },
-    {
-        name: "Game",
-        icon: faGamepad,
-        link: "/admin/game",
-    },
-    {
-        name: "Denom",
-        icon: faCube,
-        link: "/admin/denom",
-    },
-    // {
-    //     name: "Kategori Denom",
-    //     icon: faCube,
-    //     link: "/admin/product-category",
-    // },
-    {
-        name: "Voucher Game",
-        icon: faGamepad,
-        link: "/admin/game-voucher",
-    },
-    {
-        name: "Metode Pembayaran",
-        icon: faCreditCard,
-        link: "/admin/payment-method",
-    },
-    // {
-    //     name: "Postingan",
-    //     icon: faGlobeAsia,
-    //     link: "/admin/posts",
-    // },
-    {
-        name: "Media Sosial",
-        icon: faContactCard,
-        link: "/admin/social-media",
-    },
-    {
-        name: "Youtube Video",
-        icon: faYoutube,
-        link: "/admin/youtube",
-    },
-    {
-        name: "Riwayat Pesanan",
-        icon: faHistory,
-        link: "/admin/orders",
-    },
-    {
-        name: "Riwayat Deposit",
-        icon: faHistory,
-        link: "/admin/deposit-history",
-    },
+  {
+    name: "Admin",
+    icon: faHome,
+    link: "/admin/admin",
+  },
+  {
+    name: "Dashboard",
+    icon: faHome,
+    link: "/admin",
+  },
+  {
+    name: "Konfigurasi",
+    icon: faCogs,
+    link: "/admin/configuration",
+  },
+  {
+    name: "Banner",
+    icon: faImage,
+    link: "/admin/banner",
+  },
+  {
+    name: "Kode Promo",
+    icon: faTicket,
+    link: "/admin/promo-code",
+  },
+  {
+    name: "User",
+    icon: faUser,
+    link: "/admin/user",
+  },
+  {
+    name: "Reseller",
+    icon: faDiagramProject,
+    link: "/admin/reseller",
+  },
+  {
+    name: "Game",
+    icon: faGamepad,
+    link: "/admin/game",
+  },
+  {
+    name: "Denom",
+    icon: faCube,
+    link: "/admin/denom",
+  },
+  // {
+  //     name: "Kategori Denom",
+  //     icon: faCube,
+  //     link: "/admin/product-category",
+  // },
+  {
+    name: "Voucher Game",
+    icon: faGamepad,
+    link: "/admin/game-voucher",
+  },
+  {
+    name: "Metode Pembayaran",
+    icon: faCreditCard,
+    link: "/admin/payment-method",
+  },
+  // {
+  //     name: "Postingan",
+  //     icon: faGlobeAsia,
+  //     link: "/admin/posts",
+  // },
+  {
+    name: "Media Sosial",
+    icon: faContactCard,
+    link: "/admin/social-media",
+  },
+  {
+    name: "Youtube Video",
+    icon: faYoutube,
+    link: "/admin/youtube",
+  },
+  {
+    name: "Riwayat Pesanan",
+    icon: faHistory,
+    link: "/admin/orders",
+  },
+  {
+    name: "Riwayat Deposit",
+    icon: faHistory,
+    link: "/admin/deposit-history",
+  },
 ];
 const SideBarAdmin = () => {
-    const pathname = usePathname();
-    const router = useRouter();
-    const [currentPath, setCurrentPath] = useState("");
-    const [showMenuAdmin, setShowMenuAdmin] = useState(false);
-    const [data, setData] = useState<any>();
+  const pathname = usePathname();
+  const router = useRouter();
+  const [currentPath, setCurrentPath] = useState("");
+  const [showMenuAdmin, setShowMenuAdmin] = useState(false);
+  const [showFullSidebar, setShowFullSidebar] = useState(true);
+  const [data, setData] = useState<any>();
 
-    const [logo, setLogo] = useRecoilState(imageAtom);
+  const [logo, setLogo] = useRecoilState(imageAtom);
 
-    const getLogo = async () => {
-        const req = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/v1/config?type=logo", {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "ngrok-skip-browser-warning": "true",
-            },
-        });
-
-        const res = await req.json();
-        if (req.ok) {
-            setLogo((prev) => ({
-                ...prev,
-                logo: res[0].value,
-            }));
-        }
-    };
-
-    const handleLogout = async () => {
-        const toastId = toast.loading("Proses Logout...");
-        const responseCustomer = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/v1/admin/logout", {
-            method: "DELETE",
-            credentials: "include",
-            headers: {
-                "ngrok-skip-browser-warning": "true",
-            },
-        });
-
-        if (responseCustomer.ok) {
-            toast.update(toastId, {
-                render: "Berhasil Logout",
-                type: "success",
-                isLoading: false,
-                position: "top-right",
-                autoClose: 3000,
-            });
-            localStorage.setItem("auth-admin", JSON.stringify({ login: false }));
-            localStorage.removeItem("admin");
-            router.push("/admin/auth/login");
-        } else {
-            const res = await responseCustomer.json();
-            toast.update(toastId, {
-                render: res.message,
-                type: "error",
-                isLoading: false,
-                position: "top-right",
-                autoClose: 3000,
-            });
-        }
-    };
-
-    useEffect(() => {
-        setCurrentPath(pathname);
-    }, [pathname]);
-
-    useEffect(() => {
-        setTimeout(() => {
-            const data = JSON.parse(localStorage.getItem("admin") as any);
-            setShowMenuAdmin(data?.roleName === "super-admin");
-            setData(data);
-        }, 1000);
-
-        if (!logo.logo) {
-            getLogo();
-        }
-    }, []);
-
-    return (
-        <div className="w-full max-w-[16rem] font-montserrat flex flex-col items-center bg-gray-100 p-4 gap-3 overflow-y-scroll h-screen">
-            <div className="flex gap-5 items-center w-full my-5 flex-col">
-                <div className="aspect-square rounded-lg w-40">
-                    {logo.logo && (
-                        <Image
-                            src={logo.logo}
-                            alt="Logo Gasskeun Topup"
-                            className="rounded-lg object-contain"
-                            width="0"
-                            height="0"
-                            sizes="100vw"
-                            style={{ width: "100%", height: "100%" }}
-                        />
-                    )}
-                </div>
-                <div className="w-40">
-                    <h1 className=" text-lg font-semibold">Hallo, {data?.name || "Admin"}</h1>
-                </div>
-            </div>
-            <div>
-                {MENU.map(
-                    (menu, i) =>
-                        ((menu.link === "/admin/admin" && showMenuAdmin) || menu.link !== "/admin/admin") && (
-                            <Link
-                                key={i}
-                                href={menu.link}
-                                className={`${
-                                    currentPath === menu.link ? "bg-white shadow-lg shadow-gray-300" : "bg-transparent"
-                                } w-full p-2 rounded-lg flex gap-3 items-center hover:bg-gray-300`}
-                            >
-                                <div
-                                    className={`${
-                                        currentPath === menu.link ? "bg-[#B72025] text-white" : "bg-white"
-                                    } w-10 h-10 rounded-md flex items-center justify-center shadow-md`}
-                                >
-                                    <FontAwesomeIcon icon={menu.icon} size="1x" />
-                                </div>
-                                <p className="font-semibold text-sm">{menu.name}</p>
-                            </Link>
-                        ),
-                )}
-            </div>
-            <div className="border-t-2 border-gray-400 w-full">
-                <div
-                    onClick={handleLogout}
-                    className={`mt-4 w-full cursor-pointer p-2 rounded-lg flex gap-3 items-center hover:bg-gray-300 border-2 shadow-lg shadow-gray-300`}
-                >
-                    <div className={`w-10 h-10 rounded-md flex items-center justify-center shadow-md bg-gray-100`}>
-                        <FontAwesomeIcon icon={faDoorOpen} size="1x" />
-                    </div>
-                    <p className="font-semibold text-sm">Logout</p>
-                </div>
-            </div>
-        </div>
+  const getLogo = async () => {
+    const req = await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL + "/v1/config?type=logo",
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+      }
     );
+
+    const res = await req.json();
+    if (req.ok) {
+      setLogo((prev) => ({
+        ...prev,
+        logo: res[0].value,
+      }));
+    }
+  };
+
+  const handleLogout = async () => {
+    const toastId = toast.loading("Proses Logout...");
+    const responseCustomer = await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL + "/v1/admin/logout",
+      {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+      }
+    );
+
+    if (responseCustomer.ok) {
+      toast.update(toastId, {
+        render: "Berhasil Logout",
+        type: "success",
+        isLoading: false,
+        position: "top-right",
+        autoClose: 3000,
+      });
+      localStorage.setItem("auth-admin", JSON.stringify({ login: false }));
+      localStorage.removeItem("admin");
+      router.push("/admin/auth/login");
+    } else {
+      const res = await responseCustomer.json();
+      toast.update(toastId, {
+        render: res.message,
+        type: "error",
+        isLoading: false,
+        position: "top-right",
+        autoClose: 3000,
+      });
+    }
+  };
+
+  useEffect(() => {
+    setCurrentPath(pathname);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const data = JSON.parse(localStorage.getItem("admin") as any);
+      setShowMenuAdmin(data?.roleName === "super-admin");
+      setData(data);
+    }, 1000);
+
+    if (!logo.logo) {
+      getLogo();
+    }
+  }, []);
+
+  return (
+    <div
+      className={`w-full ${
+        showFullSidebar ? "max-w-[16rem]" : "max-w-[5rem]"
+      } flex flex-col items-center bg-white gap-3 overflow-x-visible h-screen transition-all duration-700`}
+    >
+      {/* close button */}
+      <div className="sidebar-header w-full bg-white border-b border-slate-200 relative">
+        <div
+          onClick={() => {
+            setShowFullSidebar((prev) => !prev);
+          }}
+          className="text-white w-7 h-7 flex items-center justify-center rounded-full bg-primary-900 text-base absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2 z-50"
+        >
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            className={`${
+              !showFullSidebar
+                ? "rotate-180"
+                : "rotate-0 transition-all duration-700"
+            }`}
+          />
+        </div>
+
+        <Link href={"/"} className="logo w-full flex items-center gap-2 p-4">
+          <div className="aspect-square rounded-lg w-10 shrink-0">
+            {logo.logo && (
+              <Image
+                src={logo.logo}
+                alt="Logo Gasskeun Topup"
+                className="rounded-lg object-contain"
+                width="0"
+                height="0"
+                sizes="100vw"
+                style={{ width: "100%", height: "100%" }}
+              />
+            )}
+          </div>
+          <h6
+            className={`text-lg font-semibold whitespace-nowrap scale-1 opacity-1 duration-700 ${
+              !showFullSidebar && "scale-0 opacity-0 -translate-x-full"
+            }`}
+          >
+            Gasskeun Topup
+          </h6>
+        </Link>
+      </div>
+      <div className="w-full px-4 overflow-y-auto">
+        {MENU.map(
+          (menu, i) =>
+            ((menu.link === "/admin/admin" && showMenuAdmin) ||
+              menu.link !== "/admin/admin") && (
+              <Link
+                key={i}
+                href={menu.link}
+                className={`${
+                  currentPath === menu.link
+                    ? "bg-primary-900"
+                    : "bg-transparent hover:bg-primary-100"
+                } w-full py-3 px-4 rounded-md flex gap-4 items-center group duration-500 `}
+              >
+                <div
+                  className={`icon text-base transition-all duration-500 ${
+                    currentPath === menu.link
+                      ? "text-white"
+                      : "text-neutral-500 group-hover:text-primary-900"
+                  }`}
+                >
+                  <FontAwesomeIcon icon={menu.icon} />
+                </div>
+
+                <p
+                  className={`font-base text-base transition-all duration-500 ${
+                    !showFullSidebar && "scale-0 opacity-0 -translate-x-full"
+                  } ${
+                    currentPath === menu.link
+                      ? "text-white"
+                      : "text-neutral-500 group-hover:text-primary-900"
+                  }`}
+                >
+                  {menu.name}
+                </p>
+              </Link>
+            )
+        )}
+      </div>
+      {/* <div className="border-t-2 border-gray-400 w-full">
+        <div
+          onClick={handleLogout}
+          className={`mt-4 w-full cursor-pointer p-2 rounded-lg flex gap-3 items-center hover:bg-gray-300 border-2 shadow-lg shadow-gray-300`}
+        >
+          <div
+            className={`w-10 h-10 rounded-md flex items-center justify-center shadow-md bg-gray-100`}
+          >
+            <FontAwesomeIcon icon={faDoorOpen} size="1x" />
+          </div>
+          <p className="font-semibold text-sm">Logout</p>
+        </div>
+      </div> */}
+    </div>
+  );
 };
 
 export default SideBarAdmin;

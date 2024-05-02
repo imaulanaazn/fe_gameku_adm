669f2f5587dc359c9f8653032c3773655ab1d6d8
irham maulana <i.maulana.azn@gmail.com>
2024-05-02 18:56:45 +0700

    redesign konfigurasi dashboard page - unfinished

--- src/app/(admin)/admin/configuration/page.tsx
+++ src/app/(admin)/admin/configuration/page.tsx
@@ -12,86 +12,67 @@ import { useRecoilState } from "recoil";
 import Image from "next/image";
 import BackgroungImages from "@/components/admin/Configuration/BackgroungImages";
 import ChangeLogoFooter from "@/components/admin/Configuration/ChangeLogoFooter";
+import AdminNavbar from "@/components/admin/AdminNavbar/AdminNavbar";
 
 const Configuration = () => {
-    const [listTemplate, setListTemplate] = useRecoilState(whatsappTemplateState);
-    const [activeTab, setActiveTab] = useState("website");
+  const [listTemplate, setListTemplate] = useRecoilState(whatsappTemplateState);
 
-    const TABS = [
-        { id: "website", label: "Website" },
-        { id: "whatsapp", label: "Whatsapp" },
-        { id: "xendit", label: "Xendit" },
-    ];
-
-    const getListTemplate = async () => {
-        const request = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/v1/whatsapp?withContent=true", {
-            cache: "no-cache",
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "ngrok-skip-browser-warning": "true",
-            },
-        });
-
-        const res = await request.json();
-        if (request.ok) {
-            setListTemplate(res);
-        }
-    };
-
-    useEffect(() => {
-        getListTemplate();
-    }, []);
-
-    return (
-        <>
-            <Header title="Konfigurasi" />
-            <div className="w-full font-montserrat mt-5 p-5 bg-white rounded-lg shadow-lg">
-                <ul className="flex flex-wrap -mb-px">
-                    {TABS.map((tab) => (
-                        <button
-                            key={tab.id}
-                            onClick={() => setActiveTab(tab.id)}
-                            className={`${
-                                activeTab === tab.id
-                                    ? "text-[#B72025] border-b-2 border-[#B72025]"
-                                    : "border-transparent hover:text-gray-600 hover:border-gray-300"
-                            } font-montserrat font-semibold inline-block p-4 border-b-2 rounded-t-lg`}
-                        >
-                            {tab.label}
-                        </button>
-                    ))}
-                </ul>
-                {activeTab === "website" && (
-                    <div>
-                        <div className="w-full flex">
-                            <div className="mt-4 flex gap-4 w-1/2">
-                                <ChangeLogo />
-                                <StatusWebsite />
-                            </div>
-                            <div className="mt-4 flex gap-4 w-1/2">
-                                <BackgroungImages />
-                            </div>
-                        </div>
-                        <ChangeLogoFooter />
-                    </div>
-                )}
-
-                {activeTab === "whatsapp" && (
-                    <div>
-                        <Whatsapp />
-                        {listTemplate.length > 0 && <TemplateMessage />}
-                    </div>
-                )}
-
-                {activeTab === "xendit" && (
-                    <div className="mt-4 w-2/3">
-                        <Xendit />
-                    </div>
-                )}
-            </div>
-        </>
+  const getListTemplate = async () => {
+    const request = await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL + "/v1/whatsapp?withContent=true",
+      {
+        cache: "no-cache",
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+      }
     );
+
+    const res = await request.json();
+    if (request.ok) {
+      setListTemplate(res);
+    }
+  };
+
+  useEffect(() => {
+    getListTemplate();
+  }, []);
+
+  return (
+    <>
+      <AdminNavbar />
+      <div className="wrapper w-full p-8">
+        <div className="website-config bg-white shadow-md rounded-2xl p-8 mt-8 mb-8">
+          <h1 className="font-medium text-2xl text-neutral-800 w-full">
+            konfigurasi Website
+          </h1>
+          <div>
+            <div className="w-full flex">
+              <div className="mt-4 flex gap-4 w-1/2">
+                <ChangeLogo />
+                <StatusWebsite />
+              </div>
+              <div className="mt-4 flex gap-4 w-1/2">
+                <BackgroungImages />
+              </div>
+            </div>
+            <ChangeLogoFooter />
+          </div>
+        </div>
+
+        <div>
+          <Whatsapp />
+          {listTemplate.length > 0 && <TemplateMessage />}
+        </div>
+
+        <div className="mt-4 w-2/3">
+          <Xendit />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Configuration;

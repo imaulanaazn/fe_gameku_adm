20c056ee51e76d3d232e397dbd257cdd29c65fd9
irham maulana <i.maulana.azn@gmail.com>
2024-05-13 12:04:31 +0700

    redesign youtube admin dashboard page

--- src/app/(admin)/admin/youtube/page.tsx
+++ src/app/(admin)/admin/youtube/page.tsx
@@ -6,37 +6,56 @@ import TableYoutubeVideo from "@/components/admin/Youtube Video/TableYoutubeVide
 import { IImageCarouselPagination } from "@/interfaces/carousels";
 import { INewsVideosPagination } from "@/interfaces/newsVideo";
 import Loading from "./loading";
+import AdminNavbar from "@/components/admin/AdminNavbar/AdminNavbar";
 
 const Youtube = () => {
-    const [data, setData] = useState<INewsVideosPagination | null>(null);
-    const [loading, setLoading] = useState(true);
-    const getData = async () => {
-        const req = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/v1/youtube", {
-            cache: "no-cache",
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "ngrok-skip-browser-warning": "true",
-            },
-        });
+  const [data, setData] = useState<INewsVideosPagination | null>(null);
+  const [loading, setLoading] = useState(true);
+  const getData = async () => {
+    const req = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/v1/youtube", {
+      cache: "no-cache",
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "ngrok-skip-browser-warning": "true",
+      },
+    });
 
-        const res = await req.json();
-        if (req.ok) {
-            setData({ ...data, ...res });
-        }
+    const res = await req.json();
+    if (req.ok) {
+      setData({ ...data, ...res });
+    }
 
-        setLoading(false);
-    };
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        getData();
-    }, []);
-    return (
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+      {loading && <Loading />}
+      {!loading && (
         <>
-            {loading && <Loading />}
-            {!loading && <>{data && <TableYoutubeVideo data={data} />}</>}
+          <AdminNavbar />
+          <div className="iq-navbar-header h-48 bg-[url('/images/bg-header-abstract.jpg')] bg-cover rounded-b-3xl text-white px-12 pt-10">
+            <div className="flex justify-between items-center">
+              <div>
+                <h1 className="text-4xl font-semibold">Halo Admin</h1>
+                <p className="text-base mt-2">
+                  Selamat datang di dashboard, semoga bisnis anda berjalan
+                  lancar dan terus berkembang.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="stats-wrapper w-full px-8 -mt-10 mb-8">
+            {data && <TableYoutubeVideo data={data} />}
+          </div>
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default Youtube;
